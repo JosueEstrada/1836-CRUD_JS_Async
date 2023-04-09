1f2014859fd85363a39d3caf57eccf3d9b58c0d1
Josué Estrada <dev.josuestrd@gmail.com>
2023-04-09 14:58:42 -0500

    Agregado perfil con javascript

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -1,4 +1,35 @@
 console.log('client-service');
+const crearNuevaLinea = (nombre, email) => {
+    const linea = document.createElement('tr');
+    const contenido =`
+        <td class="td" data-td>${nombre}</td>
+        <td>${email}</td>
+        <td>
+            <ul class="table__button-control">
+                <li>
+                    <a
+                        href="../screens/editar_cliente.html"
+                        class="simple-button simple-button--edit"
+                    >Editar</a
+                    >
+                </li>
+                <li>
+                    <button
+                        class="simple-button simple-button--delete"
+                        type="button"
+                    >
+                        Eliminar
+                    </button>
+                </li>
+            </ul>
+    `;
+    linea.innerHTML = contenido;
+    return linea;
+};
+
+const table = document.querySelector('[data-table]');
+
+
 const http = new XMLHttpRequest();
 
 // Abrir http (método, url)
@@ -10,9 +41,15 @@ const http = new XMLHttpRequest();
 http.open('GET', 'http://localhost:5000/perfil');
 http.send();
 http.onload = () => {
-    const data = http.response;
+    //const data = http.response;
+    // console.log(data); // Esto no es un objeto, solo es texto plano
+    // Parsear el data a un objeto tipo JSON
+    const data = JSON.parse(http.response);
     console.log(data);
+    data.forEach( perfil => {
+        const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
+        table.appendChild(nuevaLinea);
+    });
+};
 
-}
-
-console.log(http);
+// console.log(http);
