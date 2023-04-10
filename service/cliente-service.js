8e34e9f4ac67c223931389960c0c585e9fca21d1
Josué Estrada <dev.josuestrd@gmail.com>
2023-04-09 19:22:12 -0500

    Agregado promise

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -30,26 +30,33 @@ const crearNuevaLinea = (nombre, email) => {
 const table = document.querySelector('[data-table]');
 
 
-const http = new XMLHttpRequest();
+const listaClientes = () => {
+    const promise = new  Promise((resolve,reject) => {
+        const http = new XMLHttpRequest();
+        // Abrir http (método, url)
+        http.open('GET', 'http://localhost:5000/perfil');
+        http.send();
+        http.onload = () => {
+            //const data = http.response;
+            // console.log(data); // Esto no es un objeto, solo es texto plano
+            // Parsear el data a un objeto tipo JSON
+            const response = JSON.parse(http.response);
+            if (http.status >= 400){
+                reject(response);
+            } else{
+                resolve(response);
+            }
+        };
+    });
+    return promise;
+};
 
-// Abrir http (método, url)
-// CRUD
-// Create   POST
-// Read     GET
-// Update   PUT/PATCH
-// Delete   DELETE
-http.open('GET', 'http://localhost:5000/perfil');
-http.send();
-http.onload = () => {
-    //const data = http.response;
-    // console.log(data); // Esto no es un objeto, solo es texto plano
-    // Parsear el data a un objeto tipo JSON
-    const data = JSON.parse(http.response);
+console.log(listaClientes());
+listaClientes().then((data) => {
     console.log(data);
     data.forEach( perfil => {
         const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
         table.appendChild(nuevaLinea);
     });
-};
+}).catch((error) => alert("Ocurrió un error"));
 
-// console.log(http);
